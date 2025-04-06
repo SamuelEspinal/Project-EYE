87a80b031abd3731984aa6a0d924f54a17d32ab9
Darren Yu <darrenyu12@gmail.com>
2025-04-05 22:56:14 -0400

    Implemented nullbot reminder functionality

---
+++ src/nullbot_db_setup.py
@@ -0,0 +1,48 @@
+# nullbot_db_setup.py
+from dotenv import load_dotenv
+
+# Importing module 
+import mysql.connector
+
+# Creating connection object
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "nullbot",
+    password = "nullbotSQL2025!"
+)
+
+# Cursor to execute statements
+cursor = mydb.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS nullbot;")
+cursor.execute("USE nullbot;")
+
+# Drop tables
+cursor.execute("DROP TABLE reminders;")
+cursor.execute("DROP TABLE context;")
+cursor.execute("DROP TABLE users;")
+
+# Create tables
+cursor.execute("CREATE TABLE users ("
+               "user_id int PRIMARY KEY AUTO_INCREMENT,"
+               "user_name varchar(255) NOT NULL, "
+               "discord_id bigint UNIQUE NOT NULL"
+               ");")
+cursor.execute("CREATE UNIQUE INDEX i_user_id ON users (user_id);")
+
+cursor.execute("CREATE TABLE context ("
+               "context_id int PRIMARY KEY AUTO_INCREMENT,"
+               "user_id int NOT NULL,"
+               "guild_id bigint NOT NULL,"
+               "channel_id bigint NOT NULL,"
+               "FOREIGN KEY (user_id) REFERENCES users (user_id)"
+               ");")
+cursor.execute("CREATE UNIQUE INDEX i_context_id ON context (context_id);")
+
+cursor.execute("CREATE TABLE reminders ("
+               "reminder_id int PRIMARY KEY AUTO_INCREMENT,"
+               "reminder_text varchar(255) NOT NULL,"
+               "reminder_date datetime NOT NULL,"
+               "context_id int NOT NULL,"
+               "FOREIGN KEY (context_id) REFERENCES context (context_id)"
+               ");")
+cursor.execute("CREATE UNIQUE INDEX i_reminder_id ON reminders (reminder_id);")
